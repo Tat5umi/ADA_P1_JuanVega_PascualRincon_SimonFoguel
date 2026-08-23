3d986a1fdc17dd356fc9636498b296c20e5d7d5b
Simon Foguel Giraldo <sfoguelg@eafit.edu.co>
2026-08-23 08:45:52 -0500

    Update and rename undored.hpp to undoredo.hpp

--- src/undored.hpp
+++
@@ -1,7 +0,0 @@
-#ifndef UNDOREDO_HPP
-#define UNDOREDO_HPP
-
-#include <string>
-#include <stdexcept>
-#include <iostream>
-#endif
---
+++ src/undoredo.hpp
@@ -0,0 +1,23 @@
+#ifndef UNDOREDO_HPP
+#define UNDOREDO_HPP
+
+#include <string>
+#include <stdexcept>
+#include <iostream>
+
+class Documento{
+      public:
+              Documento() = default; 
+              void Modificar(size_t pos, size_t cuantosQuitar, const std::string& textoNuevo) {
+              if (pos > texto.size()){
+                throw std::runtime_error ("La posición no esta en el documento");
+              }
+              if (pos + cuantosQuitar > texto.size()){
+                throw std::runtime_error ("La cantidad a eliminar excede el numero de caracteres del documento");
+                }
+               texto.replace(pos, cuantosQuitar, textoNuevo);
+              }
+      private:
+              std::string texto;
+};
+#endif
