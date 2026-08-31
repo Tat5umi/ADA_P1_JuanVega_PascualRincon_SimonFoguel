5db97869374eedd2c5805c4fa7fec14650014058
Simon Foguel Giraldo <sfoguelg@eafit.edu.co>
2026-08-31 11:59:09 -0500

    Create main.cpp

---
+++ src/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include "undoredo.hpp"
+
+int main(int argc, char* argv[]){
+  if(argc < 2){
+        std::cerr << "uso: " << argv[0] << " <archivo_de_eventos>\n";
+        return 1;
+  }
+  try {
+       ResultadoLectura datos = LeerArchivo(argv[1]);
+       Motor<ArrayStack<Registro>> motor;
+       motor.Procesar(datos.evento);
+       Reportar(motor, datos.errores, std::cout);
+  } catch(const std::exception& error) {
+       std::cerr <<"error: " << error.what() << "\n";
+       return 1;
+  }
+  return 0;
+}
